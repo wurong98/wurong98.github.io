5d3e0a648f1f96d0c5341291ddca72426d8f0130
wurong <wurong@autoxing.com>
2025-08-05 09:57:59 +0800

    fix

--- tools/new_post.py
+++ tools/new_post.py
@@ -18,7 +18,7 @@ math: true # 是否启用数学公式支持
 def get_next_post_filename(posts_dir, date_prefix):
     existing_ids = []
     for filename in os.listdir(posts_dir):
-        if filename.startswith(date_prefix) and filename.endswith('.md'):
+        if filename.endswith('.md'):
             parts = filename.replace('.md', '').split('-p')
             if len(parts) == 2 and parts[1].isdigit():
                 existing_ids.append(int(parts[1]))
